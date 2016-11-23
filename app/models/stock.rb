1cba43f0b2e17593f4da21d4aa673e736d977473
Angie Gardi <angiegardi@yahoo.com>
2016-11-23 01:49:49 -0600

    added spinner

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,24 @@
 class Stock < ActiveRecord::Base
+# need methods to look up prices, store prices, create new stock variables that return the stock object
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
+  def self.new_from_lookup(ticker_symbol)
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    return nil unless looked_up_stock.name
+
+    new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.name)
+    new_stock.last_price = new_stock.price
+    new_stock
+  end
+
+  def price
+    closing_price = StockQuote::Stock.quote(ticker).close
+    return "#{closing_price} (closing)" if closing_price
+
+    opening_price = StockQuote::Stock.quote(ticker).open
+    return "#{opening_price} (opening)" if opening_price
+    'unavailable'
+  end
 end
