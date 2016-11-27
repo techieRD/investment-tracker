160315250be24a2e12e2656a174834feacbc4e85
Angie Gardi <angiegardi@yahoo.com>
2016-11-26 23:56:16 -0600

    fixed error with spinner by modifying spinner partial page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,14 +1,19 @@
 class StocksController < ApplicationController
   def search
-    if params[:stock]
-      @stock = Stock.find_by_ticker(params[:stock])
-      @stock ||= Stock.new_from_lookup(params[:stock])
-    end
-    if @stock
-      #render json: @stock
-      render partial: 'lookup'
-    else
-      render status: :not_found, nothing: true
-    end
+  	# when we go to search stock page, there will be stock parameter
+  	if params[:stock]
+  		@stock = Stock.find_by_ticker(params[:stock])
+  		# this symbol ||= is saying, if @stock is nil, false, undefined, assign from new_from_lookup
+  		@stock ||= Stock.new_from_lookup(params[:stock])
+  	end
+
+  	# and so, if there is something inside @stock...
+  	if @stock
+  		# a way to just check if the api call works is using json
+  		# render json: @stock
+  		render partial: 'lookup'
+  	else
+  		render status: :not_found, nothing: true
+  	end
   end
 end
