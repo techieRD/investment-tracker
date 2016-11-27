f27e901b7dbfa2979fbaf0f0b4279285dd67e732
Angie Gardi <angiegardi@yahoo.com>
2016-11-27 16:10:02 -0600

    added UI views for user updates

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,12 +1,10 @@
 class User::RegistrationsController < Devise::RegistrationsController
+    before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  before_filter :configure_permitted_parameters
+    protected
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:fist_name, :last_name)
-    devise_parameter_sanitizerfor(:account_update).push(:first_name, :last_name)
-  end
-
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+
+
   devise_for :users, :controllers => { :registrations => "user/registrations"}
-  resources :user_stocks, except: [:show, :edit, :update]
   root 'welcome#index'
+  resources :user_stocks, except: [:show, :edit, :update]
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
 
